d040b15dad8b678baedf29529435b75c04b6a424
dando90 <dando90@yahoo.it>
2022-03-07 03:21:10 +0100

    Update progetto Javascript Basics corretto

--- portfolio/javascript_basics/assets/js/script.js
+++ portfolio/javascript_basics/assets/js/script.js
@@ -1,6 +1,27 @@
 buttons=document.querySelector('#buttons');
-display=document.querySelector('#display');
-      
+let button_values = ['+','reset','-'];
+
+//Buttons creation
+function getButtons() {
+    let buttons_array = [];
+    for(let button_value of button_values) {
+      let newButton = document.createElement('button');
+      newButton.dataset.operation = button_value;
+      newButton.textContent = button_value.toString();
+      buttons_array.push(newButton);
+    }
+    return buttons_array;
+} 
+buttons.append(...getButtons());
+
+//Display creation
+let display = document.createElement('div');
+display.setAttribute('id', 'display');
+display.textContent = '0';
+displayContainer=document.querySelector('.counter-display');
+displayContainer.firstElementChild.after(display);
+
+//Counter funcionalities
 buttons.addEventListener('click', Calculation);
   
 function Calculation (event) {
